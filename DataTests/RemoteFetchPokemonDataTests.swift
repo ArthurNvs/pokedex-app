d9edfb41d8cb84470b6cb4039b3f8d1a36b69ad0
Arthur Neves <arthurnvsm@gmail.com>
2022-04-05 13:24:30 -0300

    move SUT to a factory helper method

--- DataTests/RemoteFetchPokemonDataTests.swift
+++ DataTests/RemoteFetchPokemonDataTests.swift
@@ -22,22 +22,26 @@ protocol HttpGetClient {
 class RemoteFetchPokemonDataTests: XCTestCase {
     func test_getOne_should_call_httpClient_with_correct_url() {
         let url = URL(string: "http://any-url.com")!
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteFetchPokemonData(url: url, httpClient: httpClientSpy)
+        let (sut, httpClientSpy) = makeSut(url: url)
         sut.getPokemonById(1)
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
     func test_getPokemonById_should_call_httpClient_with_correct_id() {
-        let httpClientSpy = HttpClientSpy()
+        let (sut, httpClientSpy) = makeSut()
         let id = 1
-        let sut = RemoteFetchPokemonData(url: URL(string: "http://any-url.com")!, httpClient: httpClientSpy)
         sut.getPokemonById(id)
         XCTAssertEqual(httpClientSpy.id, id)
     }
 }
 
 extension RemoteFetchPokemonDataTests {
+    func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteFetchPokemonData, httpClient: HttpClientSpy) {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteFetchPokemonData(url: url, httpClient: httpClientSpy)
+        return (sut, httpClientSpy)
+    }
+    
     public class HttpClientSpy: HttpGetClient {
         var url: URL?
         var id: Int?
