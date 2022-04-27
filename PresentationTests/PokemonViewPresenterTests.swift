7f02e1af17370a042c2e1763ef3216ecd3c7a155
Arthur Neves <arthurnvsm@gmail.com>
2022-04-26 22:17:05 -0300

    move SUT creation to a factory method

--- PresentationTests/PokemonViewPresenterTests.swift
+++ PresentationTests/PokemonViewPresenterTests.swift
@@ -24,16 +24,20 @@ struct AlertViewModel: Equatable {
 }
 
 class PokemonViewPresenterTests: XCTestCase {
-
     func test_getPokemon_should_show_error_if_id_is_not_valid() {
-        let alertViewSpy = AlertViewSpy()
-        let sut = PokemonViewPresenter(alertView: alertViewSpy)
+        let (sut, alertViewSpy) = makeSut()
         sut.getPokemon(id: 0)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Pokemon Not Found", message: "It seems you got an invalid ID here!"))
     }
 }
 
 extension PokemonViewPresenterTests {
+    func makeSut() -> (sut: PokemonViewPresenter, alertView: AlertViewSpy) {
+        let alertViewSpy = AlertViewSpy()
+        let sut = PokemonViewPresenter(alertView: alertViewSpy)
+        return (sut, alertViewSpy)
+    }
+    
     class AlertViewSpy: AlertView {
         var viewModel: AlertViewModel?
         
