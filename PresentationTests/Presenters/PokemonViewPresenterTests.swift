75efaaacfc3e908e35044498f054888eea436738
Arthur Neves <arthurnvsm@gmail.com>
2022-04-26 22:54:40 -0300

    change SUT factory to return only one object

--- PresentationTests/Presenters/PokemonViewPresenterTests.swift
+++ PresentationTests/Presenters/PokemonViewPresenterTests.swift
@@ -3,17 +3,22 @@ import Presentation
 
 class PokemonViewPresenterTests: XCTestCase {
     func test_getPokemon_should_show_error_if_id_is_not_valid() {
-        let (sut, alertViewSpy) = makeSut()
+        let alertViewSpy = AlertViewSpy()
+        let sut = makeSut(alertView: alertViewSpy)
         sut.getPokemon(id: 0)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Pokemon Not Found", message: "It seems you got an invalid ID here!"))
+    }
+    
+    func test_getPokemon_should_return_pokemon_with_valid_id() {
+        let sut = makeSut()
+        sut.getPokemon(id: 1)
     }
 }
 
 extension PokemonViewPresenterTests {
-    func makeSut() -> (sut: PokemonViewPresenter, alertView: AlertViewSpy) {
-        let alertViewSpy = AlertViewSpy()
-        let sut = PokemonViewPresenter(alertView: alertViewSpy)
-        return (sut, alertViewSpy)
+    func makeSut(alertView: AlertViewSpy = AlertViewSpy()) -> PokemonViewPresenter {
+        let sut = PokemonViewPresenter(alertView: alertView)
+        return sut
     }
     
     class AlertViewSpy: AlertView {
